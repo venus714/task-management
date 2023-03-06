ebdc4b8c499b5ad173e6b63ce5e721d27a541269
Student <venus.kabera@student.moringaschool.com>
2023-03-06 12:44:47 +0300

    dropped table

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -13,7 +13,7 @@ class AppController < Sinatra::Base
   end
 
   options "*" do
-    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS, PATCH"
     response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
     response.headers["Access-Control-Allow-Origin"] = "*"
     200
